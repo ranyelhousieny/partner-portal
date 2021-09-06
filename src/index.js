5025dd0cc96b36179a5e958eb0bc481001de24d3
Rany ElHousieny <elhousie@gmail.com>
2021-09-06 14:40:06 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
-const ProductsMicrofrontend =
-  React.lazy(() =>
-    import(
-      'ProductsMicrofrontend'
-    )
-  );
-
 const template = (
   <div>
     <h1>Partner Portal</h1>
-    <div>
-      <React.Suspense>
-        <ProductsMicrofrontend />
-      </React.Suspense>
-    </div>
   </div>
 );
 
