3229538952b669c7635bc33fd2f05739033e23c1
Rany ElHousieny <elhousie@gmail.com>
2021-09-06 06:28:26 -0700

    Start

--- src/index.js
+++ src/index.js
@@ -2,31 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
-const user = {
-  name: 'Rany ElHousieny',
-  profile: 'https://rany.tk',
-};
 
-function website(site) {
-  if (site) {
-    return site;
-  }
-  return 'Not Available';
-}
 const template = (
   <div>
     <h1>
-      Adding Sass to Webpack
+      Partner Portal
     </h1>
-    <h2>
-      {' '}
-      My Name is {user.name}
-    </h2>
-    <p>
-      Please, visit my profile
-      at{' '}
-      {website(user.profile)}
-    </p>
+    
   </div>
 );
 
